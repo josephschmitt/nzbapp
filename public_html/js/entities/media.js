8d4bd57e50d92230349d712c7de2c31a3679a0e2
Joseph Schmitt <me@joe.sh>
2014-12-25 14:55:38 -0500

    Remove outdated file

--- public_html/js/entities/media.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  (function() {
-    var jjs;
-    jjs = window.jjs = window.jjs || {};
-    return jjs.NZBAppManager.module('Media', function(Media, NZBAppManager, Backbone, Marionette, $, _) {});
-  })();
-
-}).call(this);
